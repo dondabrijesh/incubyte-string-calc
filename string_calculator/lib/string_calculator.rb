f6e24c40eeb5a713306370c6ec36e4154e970acc
dondabrijesh <dondabrijesh11@gmail.com>
2025-04-04 10:33:24 +0530

    feat : checking the delimeters before the addition

--- string_calculator/lib/string_calculator.rb
+++ string_calculator/lib/string_calculator.rb
@@ -3,5 +3,17 @@ class StringCalculator
         unless num.present?
             return 0
         end
+        deli, numbers = check_delimiter(num)
+    end
+
+    private
+
+    def check_delimiter(num)
+        if num.start_with?("//")
+            match = num.match(%r{^//(.+)\n(.*)}m)
+            [Regexp.escape(match[1]), match[2]]
+        else
+            [",", num]
+        end
     end
 end
