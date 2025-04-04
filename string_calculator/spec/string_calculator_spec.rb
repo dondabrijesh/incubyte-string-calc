d6939061740bc9334024615583d6633829101d98
dondabrijesh <dondabrijesh11@gmail.com>
2025-04-04 10:48:01 +0530

    test : More test cases added

--- string_calculator/spec/string_calculator_spec.rb
+++ string_calculator/spec/string_calculator_spec.rb
@@ -11,4 +11,20 @@ RSpec.describe StringCalculator do
     expect(calc.add("4")).to eq(4)
   end
 
+  it "adds two numbers separated by comma" do
+    expect(calc.add("2,5")).to eq(7)
+  end
+
+  it "adds multiple numbers" do
+    expect(calc.add("1,2,3,4,5")).to eq(15)
+  end
+
+  it "handles newlines as delimiters" do
+    expect(calc.add("1\n2,3")).to eq(6)
+  end
+
+  it "handles custom single-character delimiter" do
+    expect(calc.add("//;\n1;2")).to eq(3)
+  end
+
 end
