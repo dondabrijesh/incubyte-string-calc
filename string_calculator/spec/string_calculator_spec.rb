906df8adfc284598779daf65ccec17c6937a4e85
dondabrijesh <dondabrijesh11@gmail.com>
2025-04-04 10:47:35 +0530

    test : Test cases of the string calc

--- string_calculator/spec/string_calculator_spec.rb
+++ string_calculator/spec/string_calculator_spec.rb
@@ -1,1 +1,14 @@
 require_relative '../lib/string_calculator'
+
+RSpec.describe StringCalculator do
+  let(:calc) { described_class.new }
+
+  it "returns 0 for empty input" do
+    expect(calc.add("")).to eq(0)
+  end
+
+  it "returns the number for a single value" do
+    expect(calc.add("4")).to eq(4)
+  end
+
+end
